60e90900a813e3cd2821b526e64bd4fad99487d5
Ilya Beliaev <info@php-dev.info>
2020-04-10 13:46:04 +0200

    tests

--- tests/Unit/FieldsetTest.php
+++ tests/Unit/FieldsetTest.php
@@ -5,6 +5,7 @@ namespace Revolution\LaminasForm\Tests\Unit;
 
 
 use Laminas\Form\Element\Text;
+use Laminas\ServiceManager\Exception\ServiceNotFoundException;
 use Revolution\LaminasForm\Fieldset;
 use Revolution\LaminasForm\Tests\TestCase;
 
@@ -25,5 +26,28 @@ class FieldsetTest extends TestCase
 
         $output = $this->fieldset->render();
         $this->assertEquals('<fieldset><input type="text" name="text" value=""></fieldset>', $output->toHtml());
+    }
+
+    /**
+     * @param string $formHelper
+     * @testWith    ["anyOtherHelperMethodThatNoBodyWouldImplement"]
+     *              ["anyOtherHelperMethodThatNoBodyWouldImplementEver"]
+     * @throws \Laminas\ServiceManager\Exception\ServiceNotFoundException
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function testRenderThrowsServiceNotFoundExceptionOnWrongHelper(string $formHelper): void
+    {
+        $this->expectException(ServiceNotFoundException::class);
+        $this->fieldset->render($formHelper);
+    }
+
+    public function testHelperThrowsServiceNotFoundExceptionOnInvalidMethod(): void
+    {
+        $this->expectException(ServiceNotFoundException::class);
+        $helperName = uniqid('anyFakedMethod', true);
+        $this->expectExceptionMessage(
+            'A plugin by the name "' . $helperName . '" was not found in the plugin manager Laminas\View\HelperPluginManager'
+        );
+        $this->fieldset->$helperName($this->fieldset);
     }
 }
