7cbee7b0e293b7c293035ea34136af580c748d7f
Ilya Beliaev <info@php-dev.info>
2020-04-10 13:57:53 +0200

    tests

--- tests/Unit/FormTest.php
+++ tests/Unit/FormTest.php
@@ -4,7 +4,9 @@
 namespace Revolution\LaminasForm\Tests\Unit;
 
 
+use Laminas\Form\Element\Text;
 use Laminas\ServiceManager\Exception\ServiceNotFoundException;
+use Revolution\LaminasForm\Fieldset;
 use Revolution\LaminasForm\Form;
 use Revolution\LaminasForm\Tests\TestCase;
 
@@ -17,6 +19,33 @@ class FormTest extends TestCase
     {
         parent::setUp();
         $this->form = new Form();
+    }
+
+
+    public function testRenderWithElementsWorksCorrect(): void
+    {
+        $textElement = new Text('text');
+        $this->form->add($textElement);
+
+        $output = $this->form->render();
+        $this->assertEquals(
+            '<form action="" method="POST"><input type="text" name="text" value=""></form>',
+            $output->toHtml()
+        );
+    }
+
+    public function testRenderWithFieldSetsWorksCorrect(): void
+    {
+        $fieldset = new Fieldset('fieldset');
+        $textElement = new Text('text');
+        $fieldset->add($textElement);
+        $this->form->add($fieldset);
+
+        $output = $this->form->render();
+        $this->assertEquals(
+            '<form action="" method="POST"><fieldset><input type="text" name="fieldset&#x5B;text&#x5D;" value=""></fieldset></form>',
+            $output->toHtml()
+        );
     }
 
     /**
